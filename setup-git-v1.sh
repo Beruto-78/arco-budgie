71a6c9af35e70929472610d808be3c2b8d54da10
Beruto-78 <bert.krul@gmail.com>
2018-12-16 18:10:36 +0100

    first commit

---
+++ setup-git-v1.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+##################################################################################################################
+# Author	:	Bert Krul
+# Website	:	https://brutux.nl
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+# change into your name and email.
+
+git init
+git config --global user.name "Beruto-78"
+git config --global user.email "bert.krul@gmail.com"
+sudo git config --system core.editor nano
+git config --global credential.helper cache
+git config --global credential.helper 'cache --timeout=25000'
+git config --global push.default simple
+
+
+echo "################################################################"
+echo "###################    T H E   E N D      ######################"
+echo "################################################################"
